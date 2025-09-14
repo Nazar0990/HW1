56790de91350af6a2a8909cc2c3a747246b0ea3a
Nazar0990 <lisnicuknazar46@gmail.com>
2025-09-14 16:35:10 +0300

    Add files via upload

---
+++ Hw1.cpp
@@ -0,0 +1,52 @@
+//Task1
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    cout << "Oh, the red viburnum bent down in the meadow,\n"
+        "Our glorious Ukraine somehow grew sad.\n"
+        "But we will lift that red viburnum up,\n"
+        "And we will cheer our glorious Ukraine, hey-hey!\n"
+        "Performed by: Ukrainian folk song\n";
+
+}
+//________________________________________________________
+//Task2
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Every\n"
+        "\tHunter\n"
+        "\t\tWants\n"
+        "\t\t\tTo\n"
+        "\t\t\t\tKnow\n"
+        "\t\t\t\t\tWhere\n"
+        "\t\t\t\t\t\tDoes\n"
+        "\t\t\t\t\t\t\tPheasant\n"
+        "\t\t\t\t\t\t\t\tSit\n";
+
+}
+
+//________________________________________________________
+//Task3
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Car for sale.\n"
+        "Model: McLaren P1.\n"
+        "Year: 2024\n"
+        "Color: Red\n"
+        "Engine: Hybrid V6, 680 HP\n"
+        "Fuel type: Gasoline + Electric\n"
+        "Transmission: 8-speed automatic\n"
+        "Mileage: 5000 km\n\n"
+        "Contact phone: 098-765-43-21\n";
+
+
+}
